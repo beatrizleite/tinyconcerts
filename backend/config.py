75e97cf64c92f5d0f12840b05318ed895b64e337
Beatriz Leite <41204@ufp.edu.pt>
2025-05-19 23:28:20 +0100

    Updated config.py

--- backend/config.py
+++ backend/config.py
@@ -5,5 +5,5 @@ load_dotenv()
 
 FLASK_RUN_HOST = os.getenv("FLASK_RUN_HOST", "0.0.0.0")
 FLASK_RUN_PORT = os.getenv("FLASK_RUN_PORT", 5000)
-DATABASE_URL = os.getenv("DB_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
