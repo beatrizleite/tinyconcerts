9e056120533c7090a1c17f72e7067973409a8159
Beatriz Leite | EGITRON <Beatriz.Leite@egitron.pt>
2025-05-21 11:10:38 +0100

    updated path

--- backend/tests/test_user.py
+++ backend/tests/test_user.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'backend')))
+
 import pytest
 from services.user_service import UserService
 from repositories.user_repo import UserRepo
