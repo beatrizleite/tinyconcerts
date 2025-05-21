c5db69c922800da30853ad8cdd15e9e98bc3554d
Beatriz Leite | EGITRON <Beatriz.Leite@egitron.pt>
2025-05-21 14:49:49 +0100

    - removed config for swagger from app.py since it's all in the yaml file

--- backend/app.py
+++ backend/app.py
@@ -9,21 +9,7 @@ from database import init_db
 
 def create_app(test_config=None):
     app = Flask(__name__)
-    Swagger(app, template_file='swagger/api_docs.yaml', config={
-        "headers": [],
-        "specs": [
-            {
-                "endpoint": 'apispec',
-                "route": '/apispec.json',
-                "rule_filter": lambda rule: True,
-                "model_filter": lambda tag: True,
-            }
-        ],
-        "swagger_ui": True,
-        "specs_route": "/apidocs/",
-        "host": "tinyconcerts.onrender.com",
-        "schemes": ["https"]
-    })
+    Swagger(app, template_file='swagger/api_docs.yaml')
 
     CORS(app, resources={r"/api/*": {"origins": "*"}},
          supports_credentials=True)
