14f3605a35dee5d6a9f3c7e13f30da69af532713
Beatriz Leite | EGITRON <Beatriz.Leite@egitron.pt>
2025-05-21 14:59:01 +0100

    - database wasn't being accessed via the repo since there was no initializer - some code linting

--- backend/database.py
+++ backend/database.py
@@ -6,21 +6,24 @@ from models.base import Base
 engine = None
 db_session = scoped_session(sessionmaker())
 
+
 def init_db(db_url=None):
     db_url = db_url or os.getenv('DATABASE_URL')
 
     if not db_url:
         raise ValueError("DATABASE_URL is not set!")
-        
+
     global engine, db_session
 
-    from models import user, video, achievement, comment, like, playlist, playlist_video, rating, report
+    # from models import user, video, achievement, comment, like, playlist,
+    # playlist_video, rating, report
 
     engine = create_engine(db_url)
     db_session = scoped_session(sessionmaker(bind=engine))
     Base.query = db_session.query_property()
 
     Base.metadata.create_all(bind=engine)
+
 
 def drop_db():
     global engine
--- backend/repositories/user_repo.py
+++ backend/repositories/user_repo.py
@@ -1,29 +1,30 @@
 from models.user import User
-from sqlalchemy.orm import Session
+from database import db_session
+
 
 class UserRepo:
-    def __init__(self, db_session: Session):
+    def __init__(self):
         self.db = db_session
 
     def getById(self, user_id: int):
         return self.db.query(User).get(user_id)
-    
+
     def getAll(self):
         return self.db.query(User).all()
-    
+
     def getByUsername(self, username: str):
         return self.db.query(User).filter(User.username == username).first()
-    
+
     def create(self, user: User):
         self.db.add(user)
         self.db.commit()
         self.db.refresh(user)
         return user
-    
+
     def update(self, user: User):
         self.db.commit()
         return user
-    
+
     def delete(self, user: User):
         self.db.delete(user)
         self.db.commit()
