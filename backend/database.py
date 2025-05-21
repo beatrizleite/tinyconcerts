3bf2fa8a1746ff5d473e858d07d0dd8ed1789cfe
Beatriz Leite <41204@ufp.edu.pt>
2025-05-21 12:35:40 +0100

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -6,7 +6,7 @@ engine = None
 db_session = scoped_session(sessionmaker())
 
 def init_db(db_url=None):
-    db_url = database_url or os.getenv('DATABASE_URL')
+    db_url = db_url or os.getenv('DATABASE_URL')
 
     if not db_url:
         raise ValueError("DATABASE_URL is not set!")
@@ -14,12 +14,6 @@ def init_db(db_url=None):
     global engine, db_session
 
     from models import user, video, achievement, comment, like, playlist, playlist_video, rating, report
-
-    if not db_url:
-        from config import DATABASE_URL
-        if not DATABASE_URL:
-            raise ValueError("DATABASE_URL is not set!")
-        db_url = DATABASE_URL
 
     engine = create_engine(db_url)
     db_session = scoped_session(sessionmaker(bind=engine))
